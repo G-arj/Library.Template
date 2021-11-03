1036a6aee8e98a40a82934619030312995eb2ae9
Andrew Arnott <andrewarnott@live.com>
2021-11-03 09:36:14 -0600

    Crash test

--- test/Library.Tests/CalculatorTests.cs
+++ test/Library.Tests/CalculatorTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using Library;
 using Xunit;
 
@@ -13,6 +14,8 @@ public class CalculatorTests
     [Fact]
     public void AddOrSubtract()
     {
+        Environment.FailFast("Test crash");
+
         // This tests aggregation of code coverage across test runs.
 #if NETCOREAPP2_1
         Assert.Equal(3, Calculator.Add(1, 2));
